a93bcae165b58954ab31ff35c23b7ef903996de6
thyag <thyago.sasso@gmail.com>
2021-07-21 18:22:12 -0300

    Comitando primeira modificação para melhorar o uso do teste.

--- Pages/LoginPage.cs
+++ Pages/LoginPage.cs
@@ -33,14 +33,14 @@ namespace TesteAvivatecCsharp.Pages
         }
         public void PreencherEmailCadastro()
         {
-            txtEmailCadastro().SendKeys("meuemail123@hotmail.com");
+            txtEmailCadastro().SendKeys("meuemail1234@hotmail.com");
             EvidencePrint("E-mail após preenchido");
             btnCriarConta().Click();
         }
 
         public void PreencherLogin()
         {
-            txtEmailLogin().SendKeys("meuemail123@hotmail.com");
+            txtEmailLogin().SendKeys("meuemail1234@hotmail.com");
             txtSenhaLogin().SendKeys("sdwssw");
             EvidencePrint("E-mail e senha após preenchimento");
             btnLogin().Click();
